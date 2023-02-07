9fc3e6c1b40abc615d2ff64940f2ef05893be0fe
Brewmium LLC <eric@brewmium.com>
2023-02-06 16:41:12 -0800

    hello world

---
+++ main.py
@@ -0,0 +1,55 @@
+import time
+import pyperclip
+
+
+def get_delay(x):
+    if x <= 14:
+        return 65
+
+    if x <= 22:
+        return 105
+
+    if x <= 24:
+        return 125
+
+    return 0
+
+
+def get_message(x):
+    if x == 6:
+        return 'Pop Paint, Villers, and Arcadia!!!'
+
+    if x == 16:
+        return 'Mechs build UC & freshen towers'
+
+    if x == 19:
+        return 'Pop Sparks!!!'
+
+    if x == 22:
+        return 'Flood the place!!!'
+
+    if x == 23:
+        return 'Teleport buffs out and back! Wait until 24 Launches for Dragon'
+
+    return ""
+
+
+#
+# Main Loops
+#
+
+print("ID Start time:", time.ctime())
+wave = 1
+while wave < 25:
+    print('Round {}'.format(wave) + ' has landed')
+    wave = wave + 1
+
+    delay = get_delay(wave)
+    message = get_message(wave)
+
+    time.sleep(delay)
+    if len(message) > 0:
+        print(message)
+        pyperclip.copy(message)
+
+print("ID is over")
